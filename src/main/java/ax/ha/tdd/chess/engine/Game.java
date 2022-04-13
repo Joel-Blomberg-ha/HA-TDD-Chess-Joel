ee70e9007ac6ff1c271efe664af413da6d01525a
Joel Blomberg <Joel.blomberg@ha.ax>
2022-04-13 11:23:22 +0300

    using split to separate the moves components into something usable

--- src/main/java/ax/ha/tdd/chess/engine/Game.java
+++ src/main/java/ax/ha/tdd/chess/engine/Game.java
@@ -37,13 +37,19 @@ public class Game {
         //! when playing the game it should be able to figure out based on what chess piece type it is how it should move.
         // and use the canMove() function of said class. but I have no idea how to make that work at the moment.
 
-        Coordinates cords = new Coordinates(move);
-        if(board.getPiece(cords).getPieceType() == PieceType.PAWN)
-        {
-            Pawn pawn = new Pawn(Player.WHITE,cords);
-            if(pawn.canMove() == true)
-            {
+        String delimiter = ",|\n";
+        String[] split = move.split(delimiter);
 
+        //! I'm aware that the code below dues not work but I imagine something like this would be the best way to do this
+
+        Coordinates cords_start = new Coordinates(split[0]);
+        Coordinates cords_end = new Coordinates(split[1]);
+        if(board.getPiece(cords_start).getPieceType() == PieceType.PAWN)
+        {
+            Pawn pawn = new Pawn(Player.WHITE,cords_start);
+            if(pawn.canMove(board,cords_end) == true)
+            {
+                pawn = new Pawn(Player.WHITE,cords_end);
             }
         }
     }
